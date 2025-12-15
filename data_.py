a9752480a75b145904524b4c7830f6c8ee3dc5f6
seoyouuun <sg541938@gmail.com>
2025-12-15 15:20:06 +0900

    Update data_.py

--- data_.py
+++ data_.py
@@ -46,3 +46,33 @@ def fetch_naver_data(query, num_data=100, client_id=None, client_secret=None):
         df['pubDate'] = pd.to_datetime(df['pubDate'], format="%a, %d %b %Y %H:%M:%S +0900")
         return df.head(num_data)
     return pd.DataFrame()
+
+def process_analysis_data(df_factor, min_count_network, min_word_len, custom_stopwords):
+    okt = Okt()
+    
+    all_text = ' '.join(df_factor['title'].tolist() + df_factor['description'].tolist())
+    all_text = re.sub(r'[^가-힣A-Za-z\s]', ' ', all_text)
+    nouns = okt.nouns(all_text)
+    
+    final_nouns = [n for n in nouns if len(n) >= min_word_len and n not in custom_stopwords]
+    word_counts = Counter(final_nouns)
+
+    node_data = []
+    doc_texts = df_factor['title'].tolist() + df_factor['description'].tolist()
+    
+    for doc in doc_texts:
+        doc_nouns = [n for n in okt.nouns(re.sub(r'[^가-힣A-Za-z\s]', ' ', doc)) if len(n) >= min_word_len and n not in custom_stopwords]
+        node_data.extend(combinations(sorted(set(doc_nouns)), 2))
+        
+    edge_counts = Counter(node_data)
+    G = nx.Graph()
+    
+    for edge, weight in edge_counts.items():
+        if weight >= min_count_network:
+            G.add_edge(edge[0], edge[1], weight=weight)
+            
+    return {
+        'df': df_factor,
+        'word_counts': word_counts,
+        'graph': G
+    }
